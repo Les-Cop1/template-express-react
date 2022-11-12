86464e714186e37e283b415e47575d449d32700c
Félix Legrelle <legrelle.f@gmail.com>
2022-11-12 22:51:37 +0100

    :heavy_plus_sign: add express-rate-limit

--- back/src/app.ts
+++ back/src/app.ts
@@ -7,11 +7,22 @@ import { Express } from '@types'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import rateLimit from 'express-rate-limit'
 import * as path from 'path'
 
 dotenv.config({ path: path.join(__dirname, '/../../.env') })
 
 const app: Express = express()
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // Limit each IP to 100 requests per 5 minutes
+})
+
+// apply rate limiter to all requests
+if (process.env.NODE_ENV !== 'test') {
+  app.use(limiter)
+}
 
 app.use(cors(getCorsOptions()))
 
